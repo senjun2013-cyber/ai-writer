2c744e3504430f42e247eae41e4678db787a612a
Calys Billion <senjun2013@gmail.com>
2026-06-09 14:17:15 +0530

    Remove forced luxe--warm class to default to light theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.classList.add('luxe--warm')
-    return () => document.documentElement.classList.remove('luxe--warm')
-  }, [])
-
   return <Component {...pageProps} />
 }
